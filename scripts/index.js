5604538372d2ab276840d366bd88d2e4cdb16f31
Lenny Remache <lenny@Lennys-MacBook-Air.local>
2022-07-26 00:52:39 -0400

    Added basic design to home page

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+// https://image.tmdb.org/t/p/original/698FjyzLdpgXmUSr63LaRwblTmx.jpg
+// https://image.tmdb.org/t/p/original/[poster_path]
+// the movie db API -> The Movie Database to retrieve movie searches
+// const apiKey2 = "3bcfcbbd6f6ebf7821effeb075ae3ed6";
+// let movie2 = "Dare Devil";
+// const temp2 = () => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/movie/popular?api_key=3bcfcbbd6f6ebf7821effeb075ae3ed6&language=en-US&page=1`
+//   )
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       return data;
+//     });
+//   s;
+// };
+
+//temp2();
